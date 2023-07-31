89f8414a7474d3d69257d3bafa240cf5e4e8c33e
sergioflag <93952303+sergioflag@users.noreply.github.com>
2023-07-31 16:03:26 -0600

    JS 44: setTimeOut - setInterval

---
+++ js/asynchrony.js
@@ -0,0 +1,30 @@
+
+// ----- JS 44: setTimeOut() - setInterval()---
+
+/* setTimeout(()=>{
+    console.log("Se ejecuta una sola vez")
+},3000); */
+
+/* setInterval(() => {
+    console.log("Se ejecuta de forma indefinida,después de un intervalo de tiempo")
+}, 1000); */
+
+/* setInterval(() => {
+    console.log(new Date().toLocaleTimeString())
+}, 1000); */
+
+/* let tempo = setTimeout(() => {
+    console.log(new Date().toLocaleTimeString())
+}, 1000);
+
+clearTimeout(tempo);
+
+console.log("Después del clear: "+tempo); */
+
+/* let interval = setInterval(() => {
+    console.log(new Date().toLocaleTimeString())
+}, 1000);
+
+clearInterval(interval);
+
+console.log("Después del clearInterval: "+interval); */
