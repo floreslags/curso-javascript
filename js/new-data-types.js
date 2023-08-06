4a79bbb5f5f621b726fd473ef5bd1a86b322b959
sergioflags <sergiofol1093@gmail.com>
2023-08-06 17:24:06 -0600

    JS 59: JSON

--- js/new-data-types.js
+++ js/new-data-types.js
@@ -391,3 +391,40 @@ const otraPersona = {
 
 otraPersona.saludar() */
 
+// ----- JS 59: JSON (JavaScript Object Notation)  -----
+
+/* const json = {
+    string:"Sergio",
+    number:29,
+    boolean:true,
+    array:["running","programing","body stretching"],
+    object:{
+        tw:"@sergiofols",
+        email:"sergiofl@mail.com"
+    },
+    null:null
+}
+
+console.log(json);
+console.log("{}");
+console.log("[]");
+
+console.log(JSON.parse("{}"));
+console.log(JSON.parse("[1,2,3]"));
+console.log(JSON.parse("true"));
+console.log(JSON.parse("false"));
+console.log(JSON.parse("null"));
+console.log(JSON.parse('{"string":"Sergio","number":29,"boolean":true,"array":["running","programing","body stretching"],"object":{"tw":"@sergiofols","email":"sergiofl@mail.com"},"null":null}'));
+
+//console.log(JSON.parse("undefined"));
+
+
+console.log(JSON.stringify([]));
+console.log(JSON.stringify({}));
+console.log(JSON.stringify([1,2,3]));
+console.log(JSON.stringify(true));
+console.log(JSON.stringify(null));
+console.log(JSON.stringify(undefined));
+console.log(JSON.stringify({x:2,y:3}));
+
+console.log(JSON.stringify(json)); */
