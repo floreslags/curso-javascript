fffc2f0c93eff7a08222a90bd69f911e7cca7b02
sergioflags <sergiofol1093@gmail.com>
2023-08-07 14:48:12 -0600

    JS 63: DOM attributes & data-attributes

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
-console.log(window)
-console.log(Document)
+// console.log(window)
+// console.log(Document)
 
 
 /* let texto = "Hola, me llamo Sergio";
@@ -53,3 +53,42 @@ console.log(document.querySelectorAll(".card"))
 console.log(document.querySelectorAll(".card")[2])
 console.log(document.querySelectorAll("#menu li"))
 console.log(document.querySelector("#menu li")) */
+
+// ----- JS 63: DOM - attributes - data-attributes  -----
+
+/* // Asignación e invocación de atributos
+
+console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href")); // Forma correcta
+
+document.documentElement.lang = "en";
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang","es-MX");
+console.log(document.documentElement.lang);
+
+const $DOMLink = document.querySelector(".link-dom");
+$DOMLink.setAttribute("target","_blank");
+$DOMLink.setAttribute("rel","noopener");
+$DOMLink.setAttribute("href","https://jonmircha.com/");
+
+// Eliminación de atributos
+console.log($DOMLink.hasAttribute("rel"));
+$DOMLink.removeAttribute("rel")
+console.log($DOMLink.hasAttribute("rel"));
+
+// Interacción con DATA-ATTRIBUTES
+console.log($DOMLink.getAttribute("data-description"));
+console.log($DOMLink.dataset);
+console.log($DOMLink.dataset.description);
+
+$DOMLink.setAttribute("data-description","Modelo de Objeto del Documento");
+console.log($DOMLink.getAttribute("data-description"));
+
+$DOMLink.dataset.description = "Suscríbete a mi blog";
+console.log($DOMLink.getAttribute("data-description"));
+console.log($DOMLink.hasAttribute("data-id"));
+$DOMLink.removeAttribute("data-id");
+console.log($DOMLink.hasAttribute("data-id")); */
