30534f7adfc96899178e3825c378837906ac3fdd
sergioflags <sergiofol1093@gmail.com>
2023-08-10 16:19:02 -0600

    JS 77: BOM - Events & Properties

--- js/dom.js
+++ js/dom.js
@@ -507,3 +507,37 @@ document.addEventListener('click',(e)=>{
         e.preventDefault();
     }
 }); */
+
+// ----- JS 77: BOM - Propiedades y eventos -----
+
+window.addEventListener('resize',(e)=>{
+    console.clear();
+    console.log('***** Evento resize *****');
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    console.log(window.outerWidth);
+    console.log(window.outerHeight);
+    console.log(e);
+});
+
+window.addEventListener('scroll',e=>{
+    console.clear();
+    console.log('***** Evento scroll *****');
+    console.log(window.scrollX);
+    console.log(window.scrollY);
+    console.log(e);
+});
+
+window.addEventListener('load',e=>{
+    console.log('***** Evento load *****');
+    console.log(window.screenX);
+    console.log(window.screenY);
+    console.log(e);
+});
+
+document.addEventListener('DOMContentLoaded',e=>{
+    console.log('***** Evento DOM content load *****');
+    console.log(window.screenX);
+    console.log(window.screenY);
+    console.log(e);
+});
