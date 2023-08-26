437badcaeb53287f13dfa354c0ea2ba269e715f1
sergioflags <sergiofol1093@gmail.com>
2023-08-26 01:10:01 -0600

    JS 107: AJAX - FETCH API

--- js/ajax.js
+++ js/ajax.js
@@ -11,14 +11,14 @@
     xhr.addEventListener("readystatechange",e=>{
 
         if(xhr.readyState !== 4) return;
-        console.log(xhr);
+        // console.log(xhr);
         
         if(xhr.status >= 200 && xhr.status < 300){
-            console.log('Éxito');
+            // console.log('Éxito');
 
             // Almacenamiento del la data
             let json = JSON.parse(xhr.responseText);
-            console.log(json);
+            // console.log(json);
 
             // Tratamiento e iteración de la data
             json.forEach(el => {
@@ -32,7 +32,7 @@
             $xhr.appendChild($fragment);
             
         }else{
-            console.log('Error');
+            // console.log('Error');
             let msg = xhr.statusText || "Ocurrión un error";
 
             $xhr.innerHTML = `Error: ${xhr.status} -- ${msg}`;
@@ -46,3 +46,32 @@
     // Envío de la petición
     xhr.send();
 })();
+
+
+(()=>{
+    const $fetch = document.getElementById('fetch'),
+    $fragment = document.createDocumentFragment();
+
+    let externo = 'https://jsonplaceholder.typicode.com/users',
+    interno = 'assets/users.json';
+
+
+    fetch(interno)
+    .then((res)=>res.ok?res.json():Promise.reject(res))
+    .then((data)=>{
+        data.forEach(el=>{
+            const $li = document.createElement('li');
+            $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`; 
+            $fragment.appendChild($li);
+        });
+
+        $fetch.appendChild($fragment);
+    })
+    .catch((err)=>{
+        let msg = err.statusText || 'Ocurrió un error';
+        $fetch.innerHTML = `Error ${err.status}: ${msg}`
+    })
+    .finally(()=>{
+        console.log('Código de ejecución independiente a la respuesta FETCH')
+    });
+})()
